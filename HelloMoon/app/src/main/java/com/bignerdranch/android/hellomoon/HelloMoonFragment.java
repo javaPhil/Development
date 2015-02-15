94f12c9f70d28333b04729d2cf22f177c3faf04b
Axsor <Axsor01@yahoo.com>
2015-02-15 13:08:44 -0500

    chapter 13

---
+++ HelloMoon/app/src/main/java/com/bignerdranch/android/hellomoon/HelloMoonFragment.java
@@ -0,0 +1,44 @@
+package com.bignerdranch.android.hellomoon;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+/**
+ * Created by Philip on 2/15/2015.
+ */
+public class HelloMoonFragment extends Fragment {
+
+    private Button mPlayButton;
+    private Button mStopButton;
+    private AudioPlayer mPlayer = new AudioPlayer();
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_hello_moon, container, false);
+
+        mPlayButton = (Button)v.findViewById(R.id.hellomoon_playButton);
+        mPlayButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mPlayer.play(getActivity());
+            }
+        });
+        mStopButton = (Button)v.findViewById(R.id.hellomoon_stopButton);
+        mStopButton.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View v){
+                mPlayer.stop();
+            }
+        });
+        return v;
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mPlayer.stop();
+    }
+}
