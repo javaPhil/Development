58f7c93af341009dba365c0f47d5e64b7d822e2f
Axsor <Axsor01@yahoo.com>
2015-02-15 10:10:16 -0500

    new class..oooooo

---
+++ CriminalIntent/app/src/main/java/com/bignerdranch/android/criminalintent/DatePickerFragment.java
@@ -0,0 +1,11 @@
+package com.bignerdranch.android.criminalintent;
+
+import android.support.v4.app.DialogFragment;
+
+/**
+ * Created by Philip on 2/15/2015.
+ */
+public class DatePickerFragment extends DialogFragment {
+
+
+}
