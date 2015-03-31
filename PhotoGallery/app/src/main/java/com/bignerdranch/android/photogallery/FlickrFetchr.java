e312f7b3a89da6a7597557c4dd147b7d2827c8d6
Axsor <Axsor01@yahoo.com>
2015-03-31 16:39:33 -0400

    Chapter 29

--- PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
+++ PhotoGallery/app/src/main/java/com/bignerdranch/android/photogallery/FlickrFetchr.java
@@ -22,6 +22,7 @@ public class FlickrFetchr {
 
     public static final String TAG = "FlickrFetchr";
     public static final String PREF_SEARCH_QUERY = "searchQuery";
+    public static final String PREF_LAST_RESULT_ID = "lastResultId";
 
     private static final String ENDPOINT = "https://api.flickr.com/services/rest/";
     private static final String API_KEY = "e011843f7569229d367a249dec7245cd";
